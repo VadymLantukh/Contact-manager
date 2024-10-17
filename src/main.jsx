4be13b3b3fc1fd8da1dc5775dbba35ca5108a7ab
Vadym_Lantukh <vadym.lantukh.1998@gmail.com>
2024-10-17 09:27:09 +0300

    update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,14 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
-import App from './App.jsx';
-import 'modern-normalize';
-import './index.css';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/integration/react';
 import { BrowserRouter } from 'react-router-dom';
+
+import App from './App.jsx';
 import { persistor, store } from './redux/store.js';
+
+import 'modern-normalize';
+import './index.css';
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
